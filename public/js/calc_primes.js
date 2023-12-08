8c4ad3587e4ddd2bc11086032bc532eddbd90c7e
ShawnS2020 <shawnathon4@gmail.com>
2023-12-08 17:40:26 -0500

    Added calc_primes

---
+++ public/js/calc_primes.js
@@ -0,0 +1,16 @@
+export default function calculatePrimes(limit) {
+  const primes = [];
+  for (let i = 2; i <= limit; i++) {
+    let isPrime = true;
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,19 @@
+import calculatePrimes from './calc_primes.js';
+
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
-    alert('Hello World!');
-    console.log('Hello World!');
+    runTest();
 });
+
+function runTest() {
+  const startTime = performance.now();
+  // Adjust this limit to preference.
+  const limit = 10000;
+  const primes = calculatePrimes(limit);
+  const endTime = performance.now();
+  
+  const duration = endTime - startTime;
+  console.log(`Calculation took ${duration} milliseconds.`);
+  console.log(`Found ${primes.length} prime numbers.`);
+}
